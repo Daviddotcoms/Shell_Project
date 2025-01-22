b4f1ac7debd5fe3a863ed61f07e76805be38c5a8
David Correa <fortniterenegeraider@gmail.com>
2025-01-22 15:56:34 -0500

    Type Command

--- app/main.ts
+++ app/main.ts
@@ -4,19 +4,28 @@ const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 function replCommand() {
   rl.question("$ ", (answer) => {
-    if (answer === "exit 0") {
+    const secParam = answer.split(" ")[1];
+    const validCommands = ["echo", "exit"];
+    const commands = answer.split(" ");
+    if (answer.includes(`type ${secParam}`)) {
+      if (validCommands.includes(secParam)) {
+        rl.write(`${secParam} is a shell builtin\n`);
+      } else {
+        rl.write(`${answer}: command not found\n`);
+      }
+    } else if (commands[0] === "echo") {
+      const slicedWords = commands.slice(1, commands.length);
+      rl.write(`${slicedWords.join(" ")}\n`);
+    } else if (answer === "exit 0") {
       rl.close();
       return 0;
-    }
-    if (answer.includes("echo")) {
-      const toPrint = answer.split(" ");
-      const slicedWords = toPrint.slice(1, toPrint.length);
-      rl.write(`${slicedWords.join(" ")}\n`);
     } else {
       rl.write(`${answer}: command not found\n`);
     }
+
     replCommand();
   });
 }
