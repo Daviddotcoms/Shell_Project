ce354a56d8077e17da4e100d2e929a92a8f85716
David Correa <fortniterenegeraider@gmail.com>
2025-01-28 21:58:05 -0500

    Rafifi test

--- app/main.ts
+++ app/main.ts
@@ -11,7 +11,7 @@ const VALID_COMMANDS: string[] = ["echo", "exit", "type"];
 
 function replCommand() {
   rl.question("$ ", (answer) => {
-    let command = answer.slice(5).trim();
+    let command = answer.slice(5);
     if (answer === "exit 0") {
       rl.close();
       return 0;
