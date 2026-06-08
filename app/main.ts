23789218076597890b07b54d4590981878c5e80f
David Correa <fortniterenegeraider@gmail.com>
2026-06-08 11:06:03 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { createInterface } from "readline";
 import fs from "fs";
 import path from "path";
+import { execSync } from "child_process";
 
 const rl = createInterface({
   input: process.stdin,
@@ -9,32 +10,46 @@ const rl = createInterface({
 
 const VALID_COMMANDS: string[] = ["echo", "exit", "type"];
 
+function findExecutable(command: string): string | null {
+  const ext = [".exe", ".bat", ".cmd", ""];
+  const paths = process.env.PATH?.split(path.delimiter) ?? [];
+
+  for (const dir of paths) {
+    for (const extention of ext) {
+      const pathExe = path.join(dir, command + extention);
+      if (fs.existsSync(pathExe)) {
+        return pathExe;
+      }
+    }
+  }
+  return null;
+}
+
 function replCommand() {
-  rl.question("$ ", (answer) => {
-    let command = answer.slice(5);
+  rl.question("$ ", (answer: string) => {
+    let [command, ...args] = answer.trim().split(" ");
     if (answer === "exit 0") {
       rl.close();
       return 0;
-    } else if (answer.startsWith("echo")) {
-      rl.write(`${command}\n`);
+    } else if (command === "echo") {
+      rl.write(`${args.join(" ")}\n`);
       return replCommand();
-    } else if (answer.startsWith("type")) {
-      if (VALID_COMMANDS.includes(command)) {
-        rl.write(`${command} is a shell builtin\n`);
+    } else if (command === "type") {
+      if (VALID_COMMANDS.includes(args[0])) {
+        rl.write(`${args[0]} is a shell builtin\n`);
       } else {
         let foundedPath: string = "";
-        const paths = process.env.PATH?.split(path.delimiter) ?? [];
-        for (const dir of paths) {
-          const pathEnv = findExecutable(dir, command);
-          if (pathEnv) {
-            foundedPath = pathEnv;
-            rl.write(`${command} is ${foundedPath}\n`);
-          }
+        const pathEnv = findExecutable(args[0]);
+        if (pathEnv !== null) {
+          foundedPath = pathEnv;
+          rl.write(`${args[0]} is ${foundedPath}\n`);
         }
         if (!foundedPath) {
-          console.log(`${command}: not found`);
+          console.log(`${args[0]}: not found`);
         }
       }
+    } else if (findExecutable(command)) {
+      execSync(answer, { stdio: "inherit" });
     } else {
       rl.write(`${answer}: not found\n`);
     }
@@ -43,14 +58,3 @@ function replCommand() {
 }
 
 replCommand();
-
-function findExecutable(dir: string, command: string): string | null {
-  const ext = [".exe", ".bat", ".cmd", ""];
-  for (const extention of ext) {
-    const pathExe = path.join(dir, command + extention);
-    if (fs.existsSync(pathExe)) {
-      return pathExe;
-    }
-  }
-  return null;
-}
